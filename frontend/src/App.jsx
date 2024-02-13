0d6a205feb81b02191704106bbab3f4caeeb92f6
Shikhar <shikharg1110@gmail.com>
2024-02-13 20:46:44 +0530

    Updated dependencies flaticons and updated tailwind css configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import Navbar from './components/navbar.component'
 
-import './App.css'
-
 export default function App() {
   return (
     <Navbar></Navbar>
--- frontend/src/components/navbar.component.jsx
+++ frontend/src/components/navbar.component.jsx
@@ -1,8 +1,9 @@
+import logo from '../imgs/logo.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-
+            <img src={logo} alt="logo" />
         </nav>
     )
 }
