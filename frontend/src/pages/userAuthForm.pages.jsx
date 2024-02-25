0cbeef8821d8ec1f8d6ed0e2dabc5c7745465df0
Shikhar <shikharg1110@gmail.com>
2024-02-25 23:48:39 +0530

    UI signIn/signUp form validation and install react-hot-toast dependencies for beaultiful notification

--- frontend/src/pages/userAuthForm.pages.jsx
+++ frontend/src/pages/userAuthForm.pages.jsx
@@ -1,13 +1,53 @@
+import { useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
 import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
 
 const UserAuthForm = ({type}) => {
+
+    const authForm = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+        // formData
+        let form = new FormData(authForm.current);
+        let formData = {};
+
+        for(let [key, value] of form.entries()) {
+            formData[key] = value;
+        }
+
+        let {fullname, email, password} = formData;
+        // form validation
+        if(fullname) {
+            if(fullname.length < 3) {
+                return toast.error("Fullname must be at least 3 characters long");
+            }
+        }
+        if(!email.length) {
+            return toast.error("Email must be entered");
+        }
+        if(!emailRegex.test(email)) {
+            return toast.error("Invalid email");
+        }
+        if(!passwordRegex.test(password)) {
+            return toast.error("Password must contain 6 to 20 characters long, contain at least 1 uppercase letter, 1 lowercase letter, and 1 number");
+    }
+
+        console.log(formData);
+    }
+
     return (
         <AnimationWrapper keyValue={type}>
             <section className="h-cover flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
+            <Toaster/>
+            <form ref={authForm} className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb24">
                     {type === "sign-in" ? "Welcome back" : "Join us today"}
                 </h1>
@@ -43,6 +83,7 @@ const UserAuthForm = ({type}) => {
                 <button
                     className="btn-dark center mt14"
                     type="submit"
+                    onClick={handleSubmit}
                 >
                     {type.replace("-", " ")}
                 </button>
