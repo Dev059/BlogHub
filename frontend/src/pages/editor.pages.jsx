c6c62a16fc4247b08cf0aa4ec354608967f9a317
Shikhar <shikharg1110@gmail.com>
2024-04-10 00:41:19 +0530

    Added EditorContext so that it contain all the data

--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -5,23 +5,40 @@ import { UserContext } from "../App";
 import { Navigate } from "react-router-dom";
 import BlogEditor from '../components/blog-editor.component';
 import PublishForm from '../components/publish-form.component';
+import { createContext } from "react";
 
+// empty structure for our blog
+const blogStructure = {
+    title: '',
+    banner: '',
+    content: [],
+    tags: [],
+    des: '', 
+    author: { personal_info: { } }
+}
+
+export const EditorContext = createContext({ });
 
 const Editor = () => {
+    // WE have to make this state so that it is parent file for Blog Editor page and Publish Draft page
+    const [ blog, setBlog ] = useState(blogStructure)
 
     const [ editorState, setEditorState ] = useState("editor"); // to show blogEditor or PublishForm 
 
     let { userAuth: { access_token } } = useContext(UserContext);
 
+    // EditorContext.Provider is like a component which has access to EditorContext of Context React Hook
     return (
         <>
-        {
-            access_token === null ? 
-            <Navigate to="/signin" /> 
-            : editorState == "editor" ? 
-            <BlogEditor /> : <PublishForm />
-        }
-
+         
+            <EditorContext.Provider value={ {blog, setBlog, editorState, setEditorState} }>
+                {
+                    access_token === null ? 
+                    <Navigate to="/signin" /> 
+                    : editorState == "editor" ? 
+                    <BlogEditor /> : <PublishForm />
+                }
+            </EditorContext.Provider>
         </>
     )
 }
